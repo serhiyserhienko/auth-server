37657c24bb9fc036bed1d8d3aa40bf04a610427a
Serhiy Serhienko <sergienkojavadev@gmail.com>
2020-01-29 18:48:32 +0200

    create base spring boot project

---
+++ src/main/java/ss/cloud/auth/server/AuthServerMicroservice.java
@@ -0,0 +1,11 @@
+package ss.cloud.auth.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthServerMicroservice {
+    public static void main(String[] args) {
+        SpringApplication.run(AuthServerMicroservice.class, args);
+    }
+}
